c352c5186ea87cb72cbccbd4744798411eaaa95a
Kara Hall <karalynn.inc@gmail.com>
2023-02-15 10:05:25 -0500

    Conditionals intro

---
+++ main.js
@@ -0,0 +1,46 @@
+if('1+1' >  5){
+    console.log("True fact!");
+} else {
+    console.log("LIES!")
+}
+if('4*5' <= 20) {
+    console.log("True Fact!");
+} else {
+    console.log("LIES!")
+}
+if ('6-2' >= 0) {
+    console.log("True Fact!") ;
+} else {
+    console.log("LIES!")
+}
+var likesDogs = true;
+if (likesDogs === true){
+    console.log("You're a dog person!");
+} else {
+    console.log("You're a cat person!")
+}
+//Greetings part 3//
+const person = {
+    name: "Stacy",
+    preferredLanguage: "Spanish",
+}
+if (person.preferredLanguage === "English") {
+    console.log("Hello, Stacy!");
+} else if (person.preferredLanguage === "Spanish"){
+    console.log("Hola, Stacy!");
+} else if (person.preferredLanguage === "French"){
+    console.log("Bonjour, Stacy!")
+}
+//Part 4 Student Grades//
+var grade = 86;
+if (grade <= 0 && grade <= 69){
+    console.log("You got an F");
+} else if (grade >= 70 && grade <= 76){
+    console.log("You got a D");
+} else if (grade >= 77 && grade <= 84) {
+    console.log("You got a C");
+} else if (grade >= 84 && grade <= 92){
+    console.log("You got a B");
+} else if (grade >= 93 && grade <= 100){
+    console.log("You got a A")
+}
